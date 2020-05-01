adac75ab05aa58c0e95e9548662f13f3c92c775c
cmeis-1840909 <cmeis@uw.edu>
2020-04-30 18:20:38 -0700

    Finished builder 4.

---
+++ slithery.py
@@ -0,0 +1,16 @@
+						
+
+
+
+
+
+
+
+									from slithery_solutions import innovate, build, impact
+
+
+									def build_slithery_solution(industry_leaders):
+									    innovate()
+									    build()
+									    impact()
+
